578919e4d09188cf8801d612c4c3b83f784d617b
christophior <chrisvillarreal1018@gmail.com>
2016-01-14 23:27:11 -0600

    moved out routes to config; fixed profile gravatar url

--- models/User.js
+++ models/User.js
@@ -64,10 +64,8 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 /**
  * Helper method for getting user's gravatar.
  */
-userSchema.methods.gravatar = function(size) {
-  if (!size) {
-    size = 200;
-  }
+userSchema.methods.gravatar = function() {
+  var size = 200;
   if (!this.email) {
     return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
   }
